fddade2a1c76c42c81d0064f70ea9afe1825354d
vidhya <vidhya.inboxme@gmail.com>
2021-01-21 14:09:06 +0530

    arrangements

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+  width: 200px;
+  display: block;
+  margin: auto;
+  text-align: center;
+
+  button {
+    margin: 20px 0;
+  }
+`;
 
 const Home = (props) => {
 
@@ -9,9 +21,13 @@ const Home = (props) => {
     }
   }
   return (
-    <div>
+    <PageWrapper>
+      <button type="button" onClick={() => navigationFunction('react_semantic_ui')}>Go to React Semantic UI</button>
+      <button type="button" onClick={() => navigationFunction('react_toolbox')}>Go to React toolbox</button>
       <button type="button" onClick={() => navigationFunction('ant_design')}>Go to Ant design</button>
-    </div>
+
+
+    </PageWrapper>
   );
 }
 
